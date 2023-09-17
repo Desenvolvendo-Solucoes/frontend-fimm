59587a9d3e49e22caf521674a88058e631f6d428
BeGoD <55290097+tfhgzeo@users.noreply.github.com>
2023-09-16 22:32:27 -0300

    fix: datagrid
    Correção do datagrid

--- src/app/solicitacoes/page.tsx
+++ src/app/solicitacoes/page.tsx
@@ -1,37 +1,21 @@
-'use client'
 import React from 'react'
 import DataGrid from '@/components/Datagrid'
 import Sidebar from '@/components/Sidebar'
 import Container from '@/components/Container'
-import SolicitacoesAction from '@/components/SolicitacoesAction'
+import { ColumnData } from '@/types'
 
-export default async function solicitacoes() {
+export default function solicitacoes() {
   const initialData = [
-    {
-      id: '1',
-      Name: 'John',
-      Age: '30',
-      Position: 'Developer',
-    },
+    { id: '1', Name: 'John', Age: '30', Position: 'Developer' },
     { id: '2', Name: 'Jane', Age: '40', Position: 'Designer' },
     // Adicione mais dados conforme necessário
   ]
 
-  const columns = [
-    { Header: 'Name', accessor: 'Name' },
-    { Header: 'Age', accessor: 'Age' },
+  const columns: ColumnData[] = [
+    { Header: 'Nome', accessor: 'Name' },
+    { Header: 'Idade', accessor: 'Age' },
     { Header: 'Position', accessor: 'Position' },
-    {
-      Header: 'Status',
-      accessor: 'status',
-    },
-    {
-      Header: 'Ações',
-      accessor: 'Ações',
-      Cell: SolicitacoesAction,
-    },
   ]
-
   return (
     <Container>
       <Sidebar screen="Solicitações" />
